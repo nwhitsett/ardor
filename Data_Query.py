a6ef65b2ac2587ed5a49b2068cce7f991533f629
nwhitsett <whitsett.n@wustl.edu>
2023-10-05 13:57:36 -0500

    Chi square filter + Export Flares to csv

--- Data_Query.py
+++ Data_Query.py
@@ -12,12 +12,20 @@ target_list = pd.read_csv('C:/Users/Nathan/OneDrive - Washington University in S
 TOI_list = target_list['Full TOI ID']
 RA = target_list['TIC Right Ascension']
 DEC = target_list['TIC Declination']
-for index in range(len(TOI_list)):
-    TOI_Name = 'TOI ' + str(TOI_list[index])
-    obs_table = Observations.query_criteria(s_ra=RA[index], s_dec=DEC[index], project="TESS", dataproduct_type = "TIMESERIES", radius="0.002 deg", calib_level = 3, target_name= str(target_list['TIC'][index]))
-    data_products = Observations.get_product_list(obs_table)
-    for data in range(len(data_products)):
-        ext_check = data_products[data][6][-6]
-        if ext_check == 'c':
-            Observations.download_products(data_products[data], download_dir = "C:/Users/Nathan/OneDrive - Washington University in St. Louis/Desktop/TESS Data/" + TOI_Name)
+M_dwarf = 0
+
+for temps in target_list['Effective Temperature Value']:
+    if temps < 4000:
+        M_dwarf += 1
+for index in range(M_dwarf):
+    try:
+        TOI_Name = 'TOI ' + str(TOI_list[index])
+        obs_table = Observations.query_criteria(s_ra=RA[index], s_dec=DEC[index], project="TESS", dataproduct_type = "TIMESERIES", radius="0.02 deg", calib_level = 3, target_name= str(target_list['TIC'][index]))
+        data_products = Observations.get_product_list(obs_table)
+        for data in range(len(data_products)):
+            ext_check = data_products[data][6][-6]
+            if ext_check == 'c':
+                Observations.download_products(data_products[data], download_dir = "C:/Users/Nathan/OneDrive - Washington University in St. Louis/Desktop/TESS Data/" + TOI_Name)
+    except:
+        continue
             
--- Flare.py
+++ Flare.py
@@ -12,22 +12,30 @@ from scipy.optimize import curve_fit
 import numpy as np
 import pandas as pd
 import statistics as st
-# from aflare.py import aflare1 as af
 
 def exp_decay(x, a, b, c):
+    '''
+    
+
+    Parameters
+    ----------
+    x : numpy array
+        Time axis data
+    a : float
+        multiplicative parameter of the exponential decay function
+    b : float
+        time constant parameter of the exponential decay function
+    c : float
+        Time-offset for the exponential decay function
+
+    Returns
+    -------
+    numpy array
+        Gives exponential decay model output with the given parameters
+
+    '''
     return a * np.exp(-b * x) + c
 
-def flare_fit(time, data):
-    popt, pcov = curve_fit(exp_decay, time, data)
-    plt.plot(time, exp_decay(time, *popt))
-    plt.plot(time, data, 'b-', label='data')
-    return popt
-
-
-def find_nearest(array, value):
-    array = np.asarray(array)
-    idx = (np.abs(array - value)).argmin()
-    return idx
 
 def TESS_FITS_csv(input_file, csv_directory, csv_name=None):
     '''
@@ -89,14 +97,15 @@ def TESS_data_extract(fits_lc_file, SAP_ERR=False, PDCSAP_ERR=False):
     sap_flux_error = hdul[1].data['SAP_FLUX_ERR']
     pdcsap_flux = hdul[1].data['PDCSAP_FLUX']
     pdcsap_flux_error = hdul[1].data['PDCSAP_FLUX_ERR']
+    pdcsap_flux_method = hdul[1].header['PDCMETHD']
     if SAP_ERR == False and PDCSAP_ERR == False:
-        return time, sap_flux, pdcsap_flux
+        return time, sap_flux, pdcsap_flux, pdcsap_flux_method
     if SAP_ERR == False and PDCSAP_ERR == True:
-        return time, sap_flux, pdcsap_flux, pdcsap_flux_error
+        return time, sap_flux, pdcsap_flux, pdcsap_flux_error, pdcsap_flux_method
     if SAP_ERR == True and PDCSAP_ERR == False:
-        return time, sap_flux, sap_flux_error, pdcsap_flux
+        return time, sap_flux, sap_flux_error, pdcsap_flux, pdcsap_flux_method
     if SAP_ERR == True and PDCSAP_ERR == True:
-        return time, sap_flux, pdcsap_flux, sap_flux_error, pdcsap_flux
+        return time, sap_flux, pdcsap_flux, sap_flux_error, pdcsap_flux, pdcsap_flux_method
 
 
 def phase_folder(time, period, epoch):
@@ -104,7 +113,7 @@ def phase_folder(time, period, epoch):
 
     Parameters
     ----------
-    time : np.array
+    time : numpy array
         The time array to be phase folded
     period : float
         The period of the planet, in the same units as the time array
@@ -113,7 +122,7 @@ def phase_folder(time, period, epoch):
 
     Returns
     -------
-    phase : np.array
+    phase : numpy array
         Numpy array of the same dimension as the input time array, but phase
         folded to the period of the planet. Transit centered at 0.
 
@@ -123,29 +132,77 @@ def phase_folder(time, period, epoch):
     phase = phase - period/2
     return phase
 
-def flare_ID(data, sigma, min_time = 3):
+def flare_ID(data, sigma):
+    '''
+    
+
+    Parameters
+    ----------
+    data : numpy array
+        Flux data for potential flares to be identified
+    sigma : float
+        The detection sensitivity for flares, in standard deviations. 
+        For example, a sigma value of 1.0 will count any data one standard
+        deviation away from the mean as a potential flare
+    min_time : int, optional
+        The cadence of expected flares, in units of the time interval of the 
+        provided data. This is to avoid counting multiple high sigma data 
+        points from the same flare as multiple flares. The default is 3.
+
+    Returns
+    -------
+    flare_indices : numpy array 
+        Outputs a list of potential flares as lists of indices in the provided
+        data. The index begins at the triggering data point.
+
+    '''
     std_dev = np.std(data)
     sigma = sigma*std_dev
     mu = np.mean(data)
     count = 0
     flare_indices = []
     flare_length = 0
+    flare_length_list = []
     flare = False
     for flux in data:
         if flux > (mu + sigma) and flare == False:
             flare = True
         if flare == True and flux > (mu+ 4*sigma):
             flare = False
-        if flare == True and data[count: (count+ min_time)].sum()/min_time > (mu +sigma):
+        if flare == True and data[count+1] > (mu +sigma)/3:
             flare_length += 1
-        if flare == True and data[count: (count+min_time)].sum()/min_time < (mu +sigma):
+        if flare == True and data[count+1] < (mu +sigma)/3:
             flare = False
             flare_indices.append(count-flare_length)
+            flare_length_list.append(flare_length)
             flare_length = 0
         count += 1
-    return flare_indices
+    return flare_indices, flare_length_list
 
 def delete_nans(time, data):
+    '''
+    
+    Parameters
+    ----------
+    time : numpy array
+        The time array to be cleared of NANs. Must be the same dimensionality
+        and 1-to-1 with the data array.
+    data : numpy array
+        The data array to be cleared of NANs. Must be the same dimensionality
+        and 1-to-1 with the time array
+
+    Returns
+    -------
+    time1 : numpy array
+        Returns the original time array, but with any NANs in the data or
+        time array removed for both arrays, at the same indices, such that
+        both arrays are still 1-to-1.
+    data1 : numpy array
+        Returns the original data array, but with any NANs in the data or
+        time array removed for both arrays, at the same indices, such that
+        both arrays are still 1-to-1
+
+    '''
     time = time.tolist()
     data = data.tolist()
     nan_set = set()
@@ -166,6 +223,23 @@ def delete_nans(time, data):
     return time1, data1
 
 def SMA_detrend(data, time_scale):
+    '''
+    
+    This applies a windowed, Single Moving Average to detrend potentially
+    periodic data.
+    Parameters
+    ----------
+    data : numpy array
+        The flux data to be detrended
+    time_scale : int
+        The time-scale to apply the detrending.
+
+    Returns
+    -------
+    numpy array
+        Returns the detrended data array.
+
+    '''
     mov_average = []
     j = 0
     i = 0
@@ -179,58 +253,73 @@ def SMA_detrend(data, time_scale):
         mov_average.append(window_average)
         i += 1
     SMA = np.array(mov_average)
-    return data - SMA
+    return (data - SMA), SMA
         
 def flare_phase_folded_ID(phase, flare_array, period, epoch):
     new_ID_list = []
     for indices in flare_array:
-        new_ID_list.append(((time[indices] - (epoch+period/2)) % period)-period/2)
+        new_ID_list.append(((phase[indices] - (epoch+period/2)) % period)-period/2)
     return np.array(new_ID_list)
 
 
-        
-file_path = 'C:/Users/Nathan/OneDrive - Washington University in St. Louis/Grad School/Spring 2023/Research/HST Cycle 31/MAST Data/K2-25 b/TESS/tess2021284114741-s0044-0000000434226736-0215-s/tess2021284114741-s0044-0000000434226736-0215-s_lc.fits'
-a, sap_flux, pdcsap_flux = TESS_data_extract('C:/Users/Nathan/OneDrive - Washington University in St. Louis/Grad School/Spring 2023/Research/HST Cycle 31/MAST Data/K2-25 b/TESS/tess2021284114741-s0044-0000000434226736-0215-s/tess2021284114741-s0044-0000000434226736-0215-s_lc.fits')
+
+file_path = 'C:/Users/Nate Whitsett/OneDrive - Washington University in St. Louis/Desktop/TESS Data/M Dwarf Hosts/TOI 122.01/tess2018206045859-s0001-0000000231702397-0120-s/tess2018206045859-s0001-0000000231702397-0120-s_lc.fits'
+a, sap_flux, pdcsap_flux, method = TESS_data_extract(file_path)
 ID = file_path[::-1]
 ID2 = ID[15:24]
 TESS_ID = ID2[::-1]
 time, flux = delete_nans(a, pdcsap_flux)
-detrend_flux = SMA_detrend(flux, 80)
+detrend_flux, detrend_array = SMA_detrend(flux, 100)
 median = st.median(detrend_flux.tolist())
 detrend_flux = detrend_flux / median
-phase = phase_folder(time, 2.7299, 1413.2)
-flares = flare_ID(detrend_flux, 5, min_time=4)
-new_flares = flare_phase_folded_ID(phase, flares, 2.7299, 1413.2)
+# phase = phase_folder(time, 2.7299, 1413.2)
+flares, lengths = flare_ID(detrend_flux, 3)
+# new_flares = flare_phase_folded_ID(phase, flares, 2.7299, 1413.2)
 
 parameter_list = []
-RSS_list = []
+chi_square = []
+# plt.plot(time, detrend_flux)
 # for events in flares:
 #     plt.axvline(time[events], c='green', linewidth=0.5)
-
+count = 0
+acceptable_flare = 0
+possible_flares = len(flares)
+dataframe_export = pd.DataFrame()
 for events in flares:
-    count = 1
+    detrend_correction = detrend_array
     normalized_time = time - time[events]
     new_time = normalized_time.tolist()
     new_data = detrend_flux.tolist()
-    new_time = np.array(new_time[events-5:events+10])
-    new_data = np.array(new_data[events-5:events+10])
-    popt, pcov, infodict, mesg, ier  = curve_fit(exp_decay, new_time[6::], new_data[6::], full_output=True, maxfev=5000)
-    plt.plot(new_time[4::]*2*24*60+4, exp_decay(new_time[4::], *popt), linestyle='dotted', label = 'Flare candidate ' + str(count) + ' model')
-    plt.scatter(new_time*2*24*60+4, new_data, s=7, label='Flare candidate ' + str(count) + ' data', color='red')
-    plt.legend()
-    plt.xlabel('Time Since Flare (min)')
-    plt.ylabel('Standardized Relative Flux')
-    plt.title('TOI ' + TESS_ID + ' Flare Index ' + str(events))
-
-    plt.show()
-    plt.clf()
-    check = input('Continue?')
-    parameter_list.append(str("a = " + str(popt[0]) + " +/- " + str(pcov[0,0]**0.5)))
-    parameter_list.append(str("b = " + str(popt[1]) + " +/- " + str(pcov[1,1]**0.5)))
-    parameter_list.append(str("c = " + str(popt[2]) + " +/- " + str(pcov[2,2]**0.5)))
-    for values in infodict['fvec']:
-        RSS = 0
-        RSS += values**2
-    RSS_list.append(list((events, RSS)))
-    count += 1
-print(RSS_list)
+    if lengths[count] >= 25 and lengths[count] <= 35:
+        new_time = np.array(new_time[events-5:events+30])*24*60
+        new_data = np.array(new_data[events-5:events+30])
+        detrend_correction = detrend_array[events-5:events+30]
+        chi2_cutoff = 20.599
+    if lengths[count] >= 15 and lengths[count] < 25:
+        new_time = np.array(new_time[events-5:events+20])*24*60
+        new_data = np.array(new_data[events-5:events+20])
+        detrend_array = detrend_array[events-5:events+20]
+        chi2_cutoff = 10.085
+    if lengths[count] > 5 and lengths[count] < 15:
+        new_time = np.array(new_time[events-5:events+10])*24*60
+        new_data = np.array(new_data[events-5:events+10])
+        detrend_correction = detrend_array[events-5:events+10]
+        chi2_cutoff = 2.833
+    else:
+        new_time = np.array(new_time[events-5:events+7])*24*60
+        new_data = np.array(new_data[events-5:events+7])
+        detrend_correction = detrend_array[events-5:events+7]
+        chi2_cutoff = 1.064
+    new_data = new_data - np.mean(new_data)
+    popt, pcov = curve_fit(exp_decay, new_time[5:15], new_data[5:15], maxfev=5000)
+    half_max = new_data[5]/2
+    amp = new_data.max()
+    FWHMt = np.log((half_max - popt[2])/popt[0])/(-popt[1]) 
+    squares = (new_data[5:15] - exp_decay(new_time[5:15], *popt))**2/(np.var(new_data[5:15]))
+    chi_squared = np.sum(squares)
+    if chi_squared < chi2_cutoff:
+        dataframe_export['Flare ' + str(events) + ' Time'] = new_time
+        dataframe_export['Flare ' + str(events) + ' Data'] = new_data
+        dataframe_export['Flare ' + str(events) + ' Detrend Correction'] = detrend_correction
+        dataframe_export.reset_index()
+        dataframe_export.to_csv('C:/Users/Nate Whitsett/Desktop/Flares/TOI 122.01/Flares.csv', index=False)
